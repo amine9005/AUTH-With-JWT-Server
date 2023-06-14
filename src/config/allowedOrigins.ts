c294ca4e6905223860dff96ff9699a99dbe1ec1b
amine9005 <amamselmitrader@gmail.com>
2023-06-14 15:44:23 +0100

    allowed origins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,7 @@
 const allowedOrigins: string[] = [
-  "http://localhost:3000",
-  "http://localhost:4200",
+  // "http://localhost:3000",
+  // "http://localhost:4200",
+  "https://mean-auth-with-jwt.onrender.com/auth/login",
 ];
 
 export default allowedOrigins;
