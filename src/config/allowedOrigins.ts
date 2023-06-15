2ddd94be323674e5a95aee6ce89398b751d35a54
amine9005 <amamselmitrader@gmail.com>
2023-06-15 10:12:15 +0100

    deployment

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,8 @@
 const allowedOrigins: string[] = [
   // "http://localhost:3000",
   // "http://localhost:4200",
+  "https://auth-system-ui.web.app",
+  "https://auth-system-ui.firebaseapp.com",
   "https://mean-auth-with-jwt.onrender.com",
 ];
 
